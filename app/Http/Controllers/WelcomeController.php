65ddd86955a20d2623d9c03f4dc51ee06d346cbf
Alexandre Sanches <alsanches@gmail.com>
2016-03-22 12:36:34 -0300

    Curso Laravel

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodeCommerce\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeCommerce\Http\Requests;
+
+class WelcomeController extends Controller
+{
+    public function index(){
+        return view('exemplo');
+    }
+}
