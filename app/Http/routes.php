273ba3e3e0553153920d9e5567acecad7a7dcbca
Alexandre Sanches <alexandre@1linha.com.br>
2016-03-23 16:11:05 -0300

    Rotas incompleto

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,34 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('exemplo', 'WelcomeController@exemplo');
+Route::put('exemplo', 'WelcomeController@exemplo');
+
+
+//Route::match(['get','post'], '/exemplo2', function(){
+//    return 'oi';
+//});
+//
+//Route::any('/exemplo2', function(){
+//    return 'Any'
+//});
 
 
 
